f8d71758897fead3f80a800aeb19f4e5d3addac0
Chris-QJ Chen <chris-qj.chen@mediatek.com>
2024-12-10 09:40:59 +0800

    GENIO: board: mediatek: Add the MT8391 EVK (genio-720-evk) board support.
    Add MT8391 EVK (genio-720-evk) board support.
    This EVK evaluation board uses MediaTek's MT8391 SoC.
    
    Change-Id: Ia46811d7ab75b33451459e5ced2789a93ade34ae
    Signed-off-by: Chris-QJ Chen <chris-qj.chen@mediatek.com>
    Signed-off-by: Macpaul Lin <macpaul.lin@mediatek.com>

---
+++ board/mediatek/mt8391/mt8391_evk.c
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2025 MediaTek Inc.
+ * Author: Chris-QJ Chen <chris-qj.chen@mediatek.com>
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <efi_loader.h>
+#include <iot_ab.h>
+#include <net.h>
+#include <asm/io.h>
+#include <linux/kernel.h>
+#include <linux/arm-smccc.h>
+
+#define MT8391_UPDATABLE_IMAGES	5
+
+#if CONFIG_IS_ENABLED(EFI_HAVE_CAPSULE_SUPPORT)
+static struct efi_fw_image fw_images[MT8391_UPDATABLE_IMAGES] = {0};
+
+struct efi_capsule_update_info update_info = {
+#if IS_ENABLED(CONFIG_MEDIATEK_IOT_AB_BOOT_SUPPORT)
+#if (IS_ENABLED(CONFIG_DFU_MTD))
+	.dfu_string = "mtd nor0=bl2.img part 1;"
+			"fip.bin part %d;firmware.vfat part %d;u-boot-env.bin part 9",
+#else
+	.dfu_string = "mmc 0=bl2.img raw 0x0 0x2000 mmcpart 1;"
+			"fip.bin part 0 %d;firmware.vfat part 0 %d;u-boot-env.bin raw 0x0 0x2000 mmcpart 2",
+#endif
+#else
+#if (IS_ENABLED(CONFIG_DFU_MTD))
+	.dfu_string = "mtd nor0=bl2.img part 1;"
+			"fip.bin part 2;firmware.vfat part 4;u-boot-env.bin part 9",
+#else
+	.dfu_string = "mmc 0=bl2.img raw 0x0 0x2000 mmcpart 1;"
+			"fip.bin part 0 1;firmware.vfat part 0 3;u-boot-env.bin raw 0x0 0x2000 mmcpart 2",
+#endif
+#endif
+	.images = fw_images,
+};
+
+u8 num_image_type_guids = MT8391_UPDATABLE_IMAGES;
+#endif
+
+#if defined(CONFIG_EFI_HAVE_CAPSULE_SUPPORT) && defined(CONFIG_EFI_PARTITION)
+enum mt8390_updatable_images {
+	MT8391_BL2_IMAGE = 1,
+	MT8391_FIP_IMAGE,
+	MT8391_FW_IMAGE,
+	MT8391_ENV_IMAGE,
+	MT8391_FIT_IMAGE,
+};
+
+void mediatek_capsule_update_board_setup(void)
+{
+	fw_images[0].image_index = MT8391_FIT_IMAGE;
+	fw_images[1].image_index = MT8391_FIP_IMAGE;
+	fw_images[2].image_index = MT8391_BL2_IMAGE;
+	fw_images[3].image_index = MT8391_FW_IMAGE;
+	fw_images[4].image_index = MT8391_ENV_IMAGE;
+
+	efi_guid_t image_type_guid = GENIO_720_EVK_FIT_IMAGE_GUID;
+	efi_guid_t uboot_image_type_guid = GENIO_720_EVK_FIP_IMAGE_GUID;
+	efi_guid_t bl2_image_type_guid = GENIO_720_EVK_BL2_IMAGE_GUID;
+	efi_guid_t fw_image_type_guid = GENIO_720_EVK_FW_IMAGE_GUID;
+	efi_guid_t env_image_type_guid = GENIO_720_EVK_ENV_IMAGE_GUID;
+
+	guidcpy(&fw_images[0].image_type_id, &image_type_guid);
+	guidcpy(&fw_images[1].image_type_id, &uboot_image_type_guid);
+	guidcpy(&fw_images[2].image_type_id, &bl2_image_type_guid);
+	guidcpy(&fw_images[3].image_type_id, &fw_image_type_guid);
+	guidcpy(&fw_images[4].image_type_id, &env_image_type_guid);
+
+	fw_images[0].fw_name = u"GENIO-720-EVK-FIT";
+	fw_images[1].fw_name = u"GENIO-720-EVK-FIP";
+	fw_images[2].fw_name = u"GENIO-720-EVK-BL2";
+	fw_images[3].fw_name = u"GENIO-720-EVK-FW";
+	fw_images[4].fw_name = u"GENIO-720-EVK-ENV";
+}
+
+#if IS_ENABLED(CONFIG_MEDIATEK_IOT_AB_BOOT_SUPPORT)
+void set_dfu_alt_info(char *interface, char *devstr)
+{
+	char alt[BOOTCTRL_DFU_ALT_LEN] = { 0 };
+	const char *s = env_get(BOOTCTRL_ENV);
+
+	if (s) {
+		if (!strcmp(s, "a")) {
+			if (sprintf(alt, update_info.dfu_string,
+				    BOOTCTRL_FIP_NUM + PART_BOOT_B,
+				    BOOTCTRL_FW_NUM + PART_BOOT_B) < 0)
+				return;
+		} else if (!strcmp(s, "b")) {
+			if (sprintf(alt, update_info.dfu_string,
+				    BOOTCTRL_FIP_NUM, BOOTCTRL_FW_NUM) < 0)
+				return;
+		}
+		env_set("dfu_alt_info", alt);
+	}
+}
+#endif
+#endif /* CONFIG_EFI_HAVE_CAPSULE_SUPPORT && CONFIG_EFI_PARTITION */
+
+int board_init(void)
+{
+	struct udevice *dev;
+	int ret;
+
+	if (CONFIG_IS_ENABLED(USB_GADGET)) {
+		ret = uclass_get_device(UCLASS_USB_GADGET_GENERIC, 0, &dev);
+		if (ret) {
+			pr_err("%s: Cannot find USB device\n", __func__);
+			return ret;
+		}
+	}
+
+	if (CONFIG_IS_ENABLED(USB_ETHER))
+		usb_ether_init();
+
+	if (IS_ENABLED(CONFIG_EFI_HAVE_CAPSULE_SUPPORT) &&
+	    IS_ENABLED(CONFIG_EFI_PARTITION))
+		mediatek_capsule_update_board_setup();
+
+	return 0;
+}
---
+++ include/configs/mt8391.h
@@ -0,0 +1,86 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for MT8391 based boards
+ *
+ * Copyright (C) 2025 MediaTek Inc.
+ * Author: Chris-QJ Chen <chris-qj.chen@mediatek.com>
+ */
+
+#ifndef __MT8391_H
+#define __MT8391_H
+
+#include <linux/sizes.h>
+
+#define CONFIG_SYS_NS16550_SERIAL
+#define CONFIG_SYS_NS16550_REG_SIZE	-4
+#define CONFIG_SYS_NS16550_MEM32
+#define CONFIG_SYS_NS16550_COM1		0x11002000
+#define CONFIG_SYS_NS16550_CLK		26000000
+
+#define GENIO_720_EVK_FIT_IMAGE_GUID \
+	EFI_GUID(0x085DDE70, 0x7863, 0x40F7, 0x86, 0x4F, \
+		 0x7E, 0xD4, 0xCA, 0x53, 0x21, 0x78)
+#define GENIO_720_EVK_FIP_IMAGE_GUID \
+	EFI_GUID(0x3DCBF27F, 0x9E63, 0x49FC, 0x93, 0x74, \
+		 0x5A, 0x62, 0x47, 0x21, 0xAD, 0x88)
+#define GENIO_720_EVK_BL2_IMAGE_GUID \
+	EFI_GUID(0x9EC54F9F, 0x077A, 0x4688, 0xBD, 0xBB, \
+		 0xB0, 0xC9, 0xD6, 0x59, 0x47, 0x78)
+#define GENIO_720_EVK_FW_IMAGE_GUID \
+	EFI_GUID(0x94089703, 0xE6C2, 0x4526, 0xB3, 0xE3, \
+		 0x2D, 0x76, 0xA6, 0x5D, 0x0E, 0xC9)
+#define GENIO_720_EVK_ENV_IMAGE_GUID \
+	EFI_GUID(0x1C56492E, 0xBE13, 0x4E8F, 0x83, 0xAC, \
+		 0x45, 0x0A, 0x0B, 0xBA, 0xB5, 0x48)
+
+/* Environment settings */
+#include <config_distro_bootcmd.h>
+
+#ifdef CONFIG_CMD_MMC
+#define BOOT_TARGET_MMC(func) func(MMC, mmc, 0)
+#else
+#define BOOT_TARGET_MMC(func)
+#endif
+
+#ifdef CONFIG_CMD_USB
+#define BOOT_TARGET_USB(func) func(USB, usb, 0)
+#else
+#define BOOT_TARGET_USB(func)
+#endif
+
+#ifdef CONFIG_CMD_SCSI
+#define BOOT_TARGET_SCSI(func) func(SCSI, scsi, 2)
+#else
+#define BOOT_TARGET_SCSI(func)
+#endif
+
+#define BOOT_TARGET_DEVICES(func) \
+	BOOT_TARGET_MMC(func) \
+	BOOT_TARGET_USB(func) \
+	BOOT_TARGET_SCSI(func)
+
+#if !defined(CONFIG_EXTRA_ENV_SETTINGS)
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"scriptaddr=0x40000000\0" \
+	"fdt_addr_r=0x44000000\0" \
+	"fdtoverlay_addr_r=0x44c00000\0" \
+	"fdt_resize=0x3000\0" \
+	"kernel_addr_r=0x45000000\0" \
+	"ramdisk_addr_r=0x46000000\0" \
+	"fdtfile=" CONFIG_DEFAULT_FDT_FILE ".dtb\0" \
+	"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"splashsource=mmc_fs\0" \
+	"splashfile=logo.bmp\0" \
+	"splashdevpart=0#bootassets\0" \
+	"splashpos=m,m\0" \
+	BOOTENV
+#endif
+
+#ifdef CONFIG_ARM64
+#define MTK_SIP_PARTNAME_ID		0xC2000529
+#endif
+
+/* DRAM */
+#define CONFIG_SYS_SDRAM_BASE		0x40000000
+
+#endif
