09d8516e42812bdf8a83d77d27c588f950d0e248
Chris-QJ Chen <chris-qj.chen@mediatek.com>
2024-12-17 09:21:22 +0800

    GENIO: mt8391: adjust dts memory size automatically
    After the BL2 stage initializes the DRAM, it will pass the
    DRAM size to the subsequent stage. Automatically configure
    the DTS memory size using passed memory size.
    
    Change-Id: I24c9e8bab5bc2fc7c91e697d54539bc309baa0fc
    Signed-off-by: Chris-QJ Chen <chris-qj.chen@mediatek.com>

--- arch/arm/include/asm/arch-mediatek/misc.h
+++ arch/arm/include/asm/arch-mediatek/misc.h
@@ -14,6 +14,15 @@
 #define APHW_VER		0x08
 #define APSW_VER		0x0c
 
+#define BOOT_ARGUMENT_LOCATION	(0x40000100)
+#define BOOT_ARGUMENT_MAGIC	0x504c504c
+#define BOOT_ARGUMENT		((struct boot_argument *)BOOT_ARGUMENT_LOCATION)
+
+struct boot_argument {
+	unsigned int magic_number;
+	unsigned long long dram_size;
+};
+
 void mediatek_capsule_update_board_setup(void);
 
 #endif /* __MEDIATEK_MISC_H_ */
