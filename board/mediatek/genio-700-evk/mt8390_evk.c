59b7d5482f8ef1311cdc67936b25590b9033d1f9
Bartosz Bilas <b.bilas@grinn-global.com>
2025-10-02 12:55:07 +0200

    board: configs: enable fixup fdt memory at runtime for GRINN boards
    Overwrite the memory size in the devicetree that is
    passed to the kernel with the actual size detected.

--- board/mediatek/genio-700-evk/mt8390_evk.c
+++ board/mediatek/genio-700-evk/mt8390_evk.c
@@ -62,6 +62,15 @@ static bool board_is_genio_700_evk(void)
 		of_machine_is_compatible("mediatek,genio-700-evk");
 }
 
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	/*
+	 * Overwrite the memory size in the devicetree that is
+	 * passed to the kernel with the actual size detected.
+	 */
+	return fdt_fixup_memory(blob, gd->ram_base, gd->ram_size);
+}
+
 static bool board_is_genio_700_evk_qspi(void)
 {
 	return CONFIG_IS_ENABLED(TARGET_MT8188) &&
